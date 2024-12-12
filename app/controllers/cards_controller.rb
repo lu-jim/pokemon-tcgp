83052017b599d71d9d8a4061a85cf0b26f02d0e8
Luis Fernando Jimenez <lu.jim@protonmail.com>
2024-12-13 00:56:21 +0100

    feat - show and new methods, basic styling with simple.css

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,17 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all()
   end
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
 end
