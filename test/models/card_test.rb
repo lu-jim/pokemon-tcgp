8b032320f2ac4f5836869fea80b2dbb571ac3443
Luis Fernando Jimenez <lu.jim@protonmail.com>
2024-12-29 03:12:24 +0100

    feat - Start testing the Card model

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save Card without a title" do
+    card = Card.new
+    assert_not card.save, "Saved the card without a title"
+  end
 end
