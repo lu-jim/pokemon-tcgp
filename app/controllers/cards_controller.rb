df59f1e464f1598e5445e523e7a5d48357e5faa1
Luis Fernando Jimenez <lu.jim@protonmail.com>
2024-12-30 23:42:03 +0100

    feat(card) - edit and update methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,15 +11,25 @@ class CardsController < ApplicationController
     @card = Card.new
   end
 
-  def edit
-  end
-
   def create
     @card = Card.new(card_params)
     if @card.save
       redirect_to @card
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
